022a0d32a321f79ba2ab358ddb304a46cbd2b91e
Shay Wagner <srwagner916@gmail.com>
2021-04-04 19:35:27 -0500

    text boxes will change color depending past present or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,50 @@
 today = dayjs().format('dddd, MMMM D');
 console.log(today);
+currentTime = dayjs().format('HH');
+console.log(currentTime);
+//turn currentTime into integer
+currentTimeInt = parseInt(currentTime);
+console.log(currentTimeInt);
 
+
+
+hoursArr = [9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17];
+console.log(hoursArr);
+
+textAreaArr = [$('#nineTextArea'),
+               $('#tenTextArea'),
+               $('#elevenTextArea'),
+               $('#twelveTextArea'),
+               $('#oneTextArea'),
+               $('#twoTextArea'),
+               $('#threeTextArea'),
+               $('#fourTextArea'),
+               $('#fiveTextArea')]
+
+
+
+             
 
 // Display today's date on page
 $('#currentDay').text(today);
+
+//color code the time blocks
+for(i=0; i<hoursArr.length; i++){
+  if (currentTimeInt === hoursArr[i]) {
+    textAreaArr[i].addClass('present');
+  }
+  else if(currentTimeInt > hoursArr[i]) {
+    textAreaArr[i].addClass('past');
+  }
+  else if(currentTimeInt < hoursArr[i]) {
+    textAreaArr[i].addClass('future')
+  }
+}
